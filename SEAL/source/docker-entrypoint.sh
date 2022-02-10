f4e6b28a1135daf6a82e68e4ea78e9b786a4bbee
Rick Weber <rick@sunscreen.tech>
2022-02-10 11:38:47 -0800

    noupload

--- SEAL/source/docker-entrypoint.sh
+++ SEAL/source/docker-entrypoint.sh
@@ -43,94 +43,22 @@ function run_microbenchmark() {
     done
 }
 
-# Microbenchmark BFV
-export OUTPUT_FILENAME=seal_bfv_microbenchmark.csv
-run_microbenchmark microbenchmark-bfv
-upload_files SEAL-BFV ${OUTPUT_FILENAME} fhe_parameters_microbenchmark_bfv.txt
-
-# Microbenchmark CKKS
-export OUTPUT_FILENAME=seal_ckks_microbenchmark.csv
-run_microbenchmark microbenchmark-ckks
-upload_files SEAL-CKKS-Batched ${OUTPUT_FILENAME} fhe_parameters_microbenchmark_ckks.txt
-
-# Cardio BFV (using modified Cingulata parameters)
-export OUTPUT_FILENAME=seal_bfv_cardio_cinguparam.csv
-run_benchmark cardio_bfv_cinguparam
-upload_files SEAL-BFV-Cinguparam ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio BFV (using Seal's automatically determined moduli)
-export OUTPUT_FILENAME=seal_bfv_cardio_sealparams.csv
-run_benchmark cardio_bfv_sealparams
-upload_files SEAL-BFV-Sealparams ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio BFV (using manually determined parameters)
-export OUTPUT_FILENAME=seal_bfv_cardio_manaulparams.csv
-run_benchmark cardio_bfv_manualparams
-upload_files SEAL-BFV-Manualparams ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-
-# Cardio BFV Naive with seal default params
-export OUTPUT_FILENAME=seal_bfv_cardio_naive_sealparams.csv
-run_benchmark cardio_bfv_naive_sealparams
-upload_files SEAL-BFV-Naive-Sealparams ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio BFV Naive with same manually selected params as manualparams
-export OUTPUT_FILENAME=seal_bfv_cardio_naive_manualparams.csv
-run_benchmark cardio_bfv_naive_manualparams
-upload_files SEAL-BFV-Naive-Manualparams ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio BFV Naive with cinguparam parameters
-export OUTPUT_FILENAME=seal_bfv_cardio_naive_cinguparam.csv
-run_benchmark cardio_bfv_naive_cinguparam
-upload_files SEAL-BFV-Naive-Cinguparam ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio BFV batched with seal default params
-export OUTPUT_FILENAME=seal_batched_bfv_cardio_sealparams.csv
-run_benchmark cardio_bfv_batched_sealparams
-upload_files SEAL-BFV-Batched-Sealparams ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio BFV batched with cinguparam
-export OUTPUT_FILENAME=seal_batched_bfv_cardio_cingupara.csv
-run_benchmark cardio_bfv_batched_cinguparam
-upload_files SEAL-BFV-Batched-Cinguparam ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio BFV batched with manual params
-export OUTPUT_FILENAME=seal_batched_bfv_cardio_manualparams.csv
-run_benchmark cardio_bfv_batched_manualparams
-upload_files SEAL-BFV-Batched-Manualparams ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# Cardio CKKS batched
-export OUTPUT_FILENAME=seal_batched_ckks_cardio.csv
-run_benchmark cardio_ckks_batched
-upload_files SEAL-CKKS-Batched ${OUTPUT_FILENAME} fhe_parameters_cardio.txt
-
-# NN CKKS batched
-export OUTPUT_FILENAME=seal_batched_ckks_nn.csv
-run_benchmark nn_ckks_batched
-upload_files SEAL-CKKS-Batched ${OUTPUT_FILENAME} fhe_parameters_nn.txt
-
 # Chi-Squared BFV with manual params, reusing subexpressions, etc (OPT)
 export OUTPUT_FILENAME=seal_bfv_chi_squared_opt.csv
 run_benchmark chi_squared_opt
-upload_files SEAL-BFV-Opt ${OUTPUT_FILENAME} fhe_parameters_chi_squared.txt
-
 
 # Chi-Squared BFV with seal params, not reusing subexpressions, etc (NAIVE)
 export OUTPUT_FILENAME=seal_bfv_chi_squared_naive.csv
 run_benchmark chi_squared_naive
-upload_files SEAL-BFV-Naive ${OUTPUT_FILENAME} fhe_parameters_chi_squared.txt
 
 # Chi-Squared BFV Batched
 export OUTPUT_FILENAME=seal_bfv_batched_chi_squared.csv
 run_benchmark chi_squared_batched
-upload_files SEAL-BFV-Batched ${OUTPUT_FILENAME} fhe_parameters_chi_squared.txt
 
 # Kernel BFV
 export OUTPUT_FILENAME=seal_bfv_kernel.csv
 run_benchmark kernel
-upload_files SEAL-BFV ${OUTPUT_FILENAME} fhe_parameters_kernel.txt
 
 # Kernel BFV batched
 export OUTPUT_FILENAME=seal_batched_bfv_kernel.csv
 run_benchmark kernel_batched
-upload_files SEAL-BFV-Batched ${OUTPUT_FILENAME} fhe_parameters_kernel.txt
