467055703bce25712261c30ce430ba0f3c731a15
EC2 Default User <ec2-user@ip-172-31-9-220.us-west-2.compute.internal>
2025-05-30 17:49:17 +0000

    Add auction and hamming

--- Cingulata/source/docker-entrypoint.sh
+++ Cingulata/source/docker-entrypoint.sh
@@ -8,57 +8,67 @@ upload_file() {
     done  
 }
 
-echo "Running chi-squared-cingulata-tfhe..."
-cd /cingu/build_tfhe/tests/tfhe/chi-squared \
+# echo "Running chi-squared-cingulata-tfhe..."
+# cd /cingu/build_tfhe/tests/tfhe/chi-squared \
+#     && chmod +x run.sh \
+#     && ./run.sh \
+#     && upload_file Cingulata-TFHE cingulata_chi_squared_tfhe.csv fhe_parameters.txt
+
+# echo "Running cardio-cingulata-tfhe..."
+# cd /cingu/build_tfhe/tests/tfhe/cardio \
+#     && chmod +x run.sh \
+#     && ./run.sh \
+#     && upload_file Cingulata-TFHE cingulata_cardio_tfhe.csv fhe_parameters.txt
+
+
+# echo "Running chi-squared..."
+# cd /cingu/eval/chi-squared \
+#     && ./run.sh \
+#     && upload_file Cingulata cingulata_chi_squared_unoptimized.csv fhe_parameters_chi_squared.txt
+
+# echo "Running cardio-cingulata..."
+# cd /cingu/eval/cardio-cingulata \
+#     && ./run.sh \
+#     && upload_file Cingulata-OPT cingulata_cardio_optimized.csv fhe_parameters.txt \
+#     && upload_file Cingulata-Baseline cingulata_cardio_unoptimized.csv fhe_parameters.txt
+
+# echo "Running cardio-lobster-baseline..."
+# cd /cingu/eval/cardio-lobster-baseline \
+#     && ./run.sh \
+#     && upload_file Lobster-Baseline cardio_lobster_baseline_unoptimized.csv fhe_parameters.txt \
+#     && upload_file Lobster-Baseline-OPT cardio_lobster_baseline_optimized.csv fhe_parameters.txt
+
+# echo "Running cardio-lobster..."
+# cd /cingu/eval/cardio-lobster \
+#     && ./run.sh \
+#     && upload_file Lobster cingulata_cardio_lobster.csv fhe_parameters.txt
+
+# echo "Running cardio-lobster-optimal-params..."
+# cd /cingu/eval/cardio-lobster-optimal-params \
+#     && ./run.sh \
+#     && upload_file Lobster-OPT-PARAMS cardio_lobster_optimal.csv fhe_parameters.txt
+
+# echo "Running cardio-multistart..."
+# cd /cingu/eval/cardio-multistart \
+#     && ./run.sh \
+#     && upload_file MultiStart cardio_multistart.csv fhe_parameters.txt
+
+# echo "Running cardio-multistart-optimal-params..."
+# cd /cingu/eval/cardio-multistart-optimal-params \
+#     && ./run.sh \
+#     && upload_file MultiStart-OPT-PARAMS cingulata_cardio_multistart_optimal.csv fhe_parameters.txt
+
+# echo "Running kernel..."
+# cd /cingu/eval/kernel \
+#     && ./run.sh \
+#     && upload_file Cingulata cingulata_kernel.csv fhe_parameters_kernel.txt
+
+# echo "Running hamming..."
+# cd /cingu/build_tfhe/tests/tfhe/hamming \
+#     && chmod +x run.sh \
+#     && ./run.sh
+
+echo "Running auction..."
+cd /cingu/build_tfhe/tests/tfhe/auction \
     && chmod +x run.sh \
-    && ./run.sh \
-    && upload_file Cingulata-TFHE cingulata_chi_squared_tfhe.csv fhe_parameters.txt
-
-echo "Running cardio-cingulata-tfhe..."
-cd /cingu/build_tfhe/tests/tfhe/cardio \
-    && chmod +x run.sh \
-    && ./run.sh \
-    && upload_file Cingulata-TFHE cingulata_cardio_tfhe.csv fhe_parameters.txt
-
-
-echo "Running chi-squared..."
-cd /cingu/eval/chi-squared \
-    && ./run.sh \
-    && upload_file Cingulata cingulata_chi_squared_unoptimized.csv fhe_parameters_chi_squared.txt
-
-echo "Running cardio-cingulata..."
-cd /cingu/eval/cardio-cingulata \
-    && ./run.sh \
-    && upload_file Cingulata-OPT cingulata_cardio_optimized.csv fhe_parameters.txt \
-    && upload_file Cingulata-Baseline cingulata_cardio_unoptimized.csv fhe_parameters.txt
-
-echo "Running cardio-lobster-baseline..."
-cd /cingu/eval/cardio-lobster-baseline \
-    && ./run.sh \
-    && upload_file Lobster-Baseline cardio_lobster_baseline_unoptimized.csv fhe_parameters.txt \
-    && upload_file Lobster-Baseline-OPT cardio_lobster_baseline_optimized.csv fhe_parameters.txt
-
-echo "Running cardio-lobster..."
-cd /cingu/eval/cardio-lobster \
-    && ./run.sh \
-    && upload_file Lobster cingulata_cardio_lobster.csv fhe_parameters.txt
-
-echo "Running cardio-lobster-optimal-params..."
-cd /cingu/eval/cardio-lobster-optimal-params \
-    && ./run.sh \
-    && upload_file Lobster-OPT-PARAMS cardio_lobster_optimal.csv fhe_parameters.txt
-
-echo "Running cardio-multistart..."
-cd /cingu/eval/cardio-multistart \
-    && ./run.sh \
-    && upload_file MultiStart cardio_multistart.csv fhe_parameters.txt
-
-echo "Running cardio-multistart-optimal-params..."
-cd /cingu/eval/cardio-multistart-optimal-params \
-    && ./run.sh \
-    && upload_file MultiStart-OPT-PARAMS cingulata_cardio_multistart_optimal.csv fhe_parameters.txt
-
-echo "Running kernel..."
-cd /cingu/eval/kernel \
-    && ./run.sh \
-    && upload_file Cingulata cingulata_kernel.csv fhe_parameters_kernel.txt
+    && ./run.sh
---
+++ Cingulata/source/hamming-cingulata-tfhe/hamming-tfhe.cxx
@@ -0,0 +1,69 @@
+/*
+    (C) Copyright 2019 CEA LIST. All Rights Reserved.
+    Contributor(s): Cingulata team (formerly Armadillo team)
+
+    This software is governed by the CeCILL-C license under French law and
+    abiding by the rules of distribution of free software.  You can  use,
+    modify and/ or redistribute the software under the terms of the CeCILL-C
+    license as circulated by CEA, CNRS and INRIA at the following URL
+    "http://www.cecill.info".
+
+    As a counterpart to the access to the source code and  rights to copy,
+    modify and redistribute granted by the license, users are provided only
+    with a limited warranty  and the software's author,  the holder of the
+    economic rights,  and the successive licensors  have only  limited
+    liability.
+
+    The fact that you are presently reading this means that you have had
+    knowledge of the CeCILL-C license and that you accept its terms.
+*/
+
+#include <vector>
+
+/* local includes */
+#include <bit_exec/decorator/attach.hxx>
+#include <bit_exec/decorator/stat.hxx>
+#include <ci_context.hxx>
+#include <ci_fncs.hxx>
+#include <ci_int.hxx>
+#include <int_op_gen/size.hxx>
+#include <tfhe_bit_exec.hxx>
+#include <int_op_gen/mult_depth.hxx>
+
+/* namespaces */
+using namespace std;
+using namespace cingulata;
+
+#define SEX_FIELD 0
+#define ANTECEDENT_FIELD 1
+#define SMOKER_FIELD 2
+#define DIABETES_FIELD 3
+#define PRESSURE_FIELD 4
+
+int main() {
+  /* Set context to tfhe bit executor and size minimized integer
+   * operations */
+  CiContext::set_config(
+      make_shared<decorator::Attach<TfheBitExec, decorator::Stat<IBitExecFHE>>>(
+          "tfhe.pk", TfheBitExec::Public),
+      make_shared<IntOpGenSize>());
+
+  // Declares variables for conditions with each 8 bit.
+  CiInt x{CiInt::u64};
+  CiInt y{CiInt::u64};
+
+  // Fills conditions with values from file.
+  x.read("x");
+  y.read("y");
+
+  CiInt x_xor_y = x ^ y;
+  CiInt count{CiInt::u8v(0)};
+  CiInt one{CiInt::u8v(1)};
+  for (int i = 0; i < 64; i++) {
+    count += (x_xor_y >> i) & one;
+  }
+
+  count.write("count");
+
+  CiContext::get_bit_exec_t<decorator::Stat<IBitExecFHE>>()->print();
+}
